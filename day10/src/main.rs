4f8a15f3e71c43aa4b43feb090c3f02eef6f48f9
Patrick Prell <prelly95@gmail.com>
2021-12-24 13:41:30 +1100

    Day 10 complete

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
 use std::fs;
 
-const INPUT: &str = "./input/test.txt";
-// const INPUT: &str = "./input/input.txt";
+// const INPUT: &str = "./input/test.txt";
+const INPUT: &str = "./input/input.txt";
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
@@ -12,20 +12,129 @@ fn main() -> Result<()> {
 	println!("\npart 1:");
 	part1(&input)?;
 	println!("\npart 2:");
-	// part2(&input)?;
+	part2(&input)?;
 	Ok(())
 }
 
 fn part1(input: &str) -> Result<()> {
-	let map:Vec<Vec<u8>> = input.lines().map(|x| x.chars().map(|y| y.to_digit(10u32).unwrap() as u8).collect()).collect();
-	println!("map:");
-	for m in map{
-		println!("{:?}", m);
+	let mut score = 0;
+	for line in input.lines() {
+		let mut open: Vec<char> = Vec::new();
+		let mut line_score = 0;
+		for c in line.chars() {
+			match c {
+				// Opening brackets
+				'(' => open.push('('),
+				'[' => open.push('['),
+				'{' => open.push('{'),
+				'<' => open.push('<'),
+				// closing brackets
+				')' => {
+					if open[open.len() - 1] == '(' {
+						open.pop();
+					} else {
+						line_score += 3;
+						break;
+					}
+				}
+				']' => {
+					if open[open.len() - 1] == '[' {
+						open.pop();
+					} else {
+						line_score += 57;
+						break;
+					}
+				}
+				'}' => {
+					if open[open.len() - 1] == '{' {
+						open.pop();
+					} else {
+						line_score += 1197;
+						break;
+					}
+				}
+				'>' => {
+					if open[open.len() - 1] == '<' {
+						open.pop();
+					} else {
+						line_score += 25137;
+						break;
+					}
+				}
+				_ => (),
+			}
+		}
+		score += line_score;
 	}
+	println!("score: {:?}", score);
 	Ok(())
 }
 
 fn part2(input: &str) -> Result<()> {
-	todo!();
+	let mut score: Vec<u64> = Vec::new();
+	for line in input.lines() {
+		let mut open: Vec<char> = Vec::new();
+		let mut corrupt = false;
+		for c in line.chars() {
+			match c {
+				// Opening brackets
+				'(' => open.push('('),
+				'[' => open.push('['),
+				'{' => open.push('{'),
+				'<' => open.push('<'),
+				// closing brackets
+				')' => {
+					if open[open.len() - 1] == '(' {
+						open.pop();
+					} else {
+						corrupt = true;
+						break;
+					}
+				}
+				']' => {
+					if open[open.len() - 1] == '[' {
+						open.pop();
+					} else {
+						corrupt = true;
+						break;
+					}
+				}
+				'}' => {
+					if open[open.len() - 1] == '{' {
+						open.pop();
+					} else {
+						corrupt = true;
+						break;
+					}
+				}
+				'>' => {
+					if open[open.len() - 1] == '<' {
+						open.pop();
+					} else {
+						corrupt = true;
+						break;
+					}
+				}
+				_ => (),
+			}
+		}
+		if !corrupt
+		{
+			let mut line_score = 0;
+			for o in open.iter().rev(){
+				match o{
+					'(' => {line_score *= 5;line_score += 1}
+					'[' => {line_score *= 5;line_score += 2}
+					'{' => {line_score *= 5;line_score += 3}
+					'<' => {line_score *= 5;line_score += 4}
+					_ => ()
+				}
+			}
+			score.push(line_score);
+		}
+	}
+	score.sort();
+	println!("score: {:?}", &score[(score.len()-1)/2]);
+
 	Ok(())
 }
